4402c5810ec428af36a9eea754d14413650da811
Emily Lemmon <eclaire.lemmon@gmail.com>
2017-08-24 15:37:23 -0500

    make the flag show and with unicode stars

---
+++ flag.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+'use strict;'
+
+const chalk = require('chalk');
+// console.log('It is \u001b[31mnot\u001b[39m easy to use \u001b[32mhardcoded \u001b[1mANSI\u001b[39m\u001b[22m codes!')
+
+
+// const bold = string => `\u001b[1m${string}\u001b[22m`;
+// const green = string => `\u001b[32m${string}\u001b[39m`;
+// const red = string => `\u001b[31m${string}\u001b[39m`;
+// console.log(`It is ${red('easier')} to use ${green('functions for')} ${green(bold('ANSI'))} codes!`);
+
+
+console.log(chalk.bgBlue.bold(` ★ ★ ★ ★ ★ ★ ★ `)+ chalk.bgRed.bold("                                  "));
+console.log(chalk.bgBlue.bold(`  ★ ★ ★ ★ ★ ★  `)+ chalk.bgWhite.bold("                                  "));
+console.log(chalk.bgBlue.bold(` ★ ★ ★ ★ ★ ★ ★ `)+ chalk.bgRed.bold("                                  "));
+console.log(chalk.bgBlue.bold(`  ★ ★ ★ ★ ★ ★  `)+ chalk.bgWhite.bold("                                  "));
+console.log(chalk.bgBlue.bold(` ★ ★ ★ ★ ★ ★ ★ `)+ chalk.bgRed.bold("                                  "));
+console.log(chalk.bgBlue.bold(`  ★ ★ ★ ★ ★ ★  `)+ chalk.bgWhite.bold("                                  "));
+console.log(chalk.bgBlue.bold(` ★ ★ ★ ★ ★ ★ ★ `)+ chalk.bgRed.bold("                                  "));
+console.log(chalk.bgWhite.bold("                                                 "));
+console.log(chalk.bgRed.bold("                                                 "));
+console.log(chalk.bgWhite.bold("                                                 "));
+console.log(chalk.bgRed.bold("                                                 "));
+console.log(chalk.bgWhite.bold("                                                 "));
+console.log(chalk.bgRed.bold("                                                 "));
